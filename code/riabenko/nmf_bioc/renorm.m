1de42f8a32dde7a52f59c41df15b8c96f7a7c760
riabenko <riabenko.e@gmail.com>
2012-06-29 20:12:58 +0400

    renorm

--- code/riabenko/nmf_bioc/renorm.m
+++ code/riabenko/nmf_bioc/renorm.m
@@ -1,56 +1,24 @@
-[A C] = nmf_smart(I,1,1000,10^-15);
-
-% figure
-% for i=1:25
-%     subplot(5,5,i)
-%     scatter(log(I(i,:)),log(C1),'Marker','.');
-%     title(strcat('a=',num2str(A1(i))))
-%     set(gca,'XLim',[1 12], 'YLim', [3 8])
-% end
-A_start = A;
-C_start = C;
-[A1_start C1_start] = normalize_prod(A,C);
-
-%A_one = zeros(size(A));
-maxIterCnt = 50;
-As = zeros(length(A), maxIterCnt+1);
-As(:,1) = A;
+function [A_new C_new isbreak] = renorm(I, A, C, maxIterCnt, eps)
+%на вход подаются матрицы интенсивностей, векторы аффинитивностей и
+%концентраций, причём sum(abs(A))==1; maxIterCnt - число проходов по
+%пробсету, eps - максимальное изменение концентраций от перенормировки, при
+%котором процесс всё ещё продолжается, isbreak - индикатор сходимости
+%концентраций.
+A_new = A;
+C_new = C;
+isbreak = false;
 for currIter = 1:maxIterCnt
-    for i=1:25
-        A_loo = A(setdiff(1:end,i)) / sum(abs(A(setdiff(1:end,i))));
+    for i=1:length(A)
+        A_loo = A_new(setdiff(1:end,i)) / sum(abs(A_new(setdiff(1:end,i))));
         C_loo = nmf_smart_fixedA(I(setdiff(1:end,i),:), A_loo,100,10^-6);
-        A(i) = nmf_smart_fixedC(I(i,:),C_loo,100,10^-6);
-        A = A / sum(abs(A));
+        A_new(i) = nmf_smart_fixedC(I(i,:),C_loo,100,10^-6);
+        A_new = A_new / sum(abs(A_new));
     end
-    As(:,currIter+1) = A;
+    C_prev = C_new;
+    C_new = nmf_smart_fixedA(I, A_new,100,10^-6);
+    if sum((C_prev-C_new).^2)<eps
+        isbreak = true;
+        break;
+    end
 end
-figure
-hold on
-for i=1:1+maxIterCnt
-    plot(1:25, As(:,i),'Color', [0 (i-1)/(1+maxIterCnt), 1-(i-1)/(1+maxIterCnt)])
-end
 
-
-
-
-C = nmf_smart_fixedA(I, A,1000,10^-15);
-figure('Name', num2str(maxIterCnt))
-subplot(2,2,1)
-scatter(A_start, A); xlabel('A\_start'); ylabel('A'); 
-xlim([0 max([A A_start])]);
-subplot(2,2,2)
-scatter(C_start, C); xlabel('C\_start'); ylabel('C');
-[A1 C1] = normalize_prod(A,C);
-subplot(2,2,3)
-scatter(A1_start, A1); xlabel('A1\_start'); ylabel('A1');
-subplot(2,2,4)
-scatter(C1_start, C1); xlabel('C1\_start'); ylabel('C1');
-
-
-figure
-for i=1:25
-    subplot(5,5,i)
-    scatter(log(I(i,:)),log(C1),'Marker','.');
-    title(strcat('a=',num2str(a_one(i))))
-    set(gca,'XLim',[1 12], 'YLim', [7 12])
-end
