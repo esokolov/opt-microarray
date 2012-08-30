da30b2555e0f27e21b3c8f016a69635758363635
esokolov <sokolov.evg@gmail.com>
2012-08-30 20:11:16 +0400

    123

--- code/riabenko/nmf_bioc/run_cv_alpha_beta_frankenstein.m
+++ code/riabenko/nmf_bioc/run_cv_alpha_beta_frankenstein.m
@@ -1,5 +1,5 @@
-alpha_range = -3:0.5:4;
-beta_range = -2:0.5:4;
+alpha_range = 0:4;
+beta_range = -4:4;
 %alpha_range = -2:0.25:2;
 %beta_range = -2:0.25:4;
 
@@ -16,6 +16,8 @@ reg_best = zeros(length(alpha_range), length(beta_range));
 A_all = cell(length(alpha_range), length(beta_range));
 B_all = cell(length(alpha_range), length(beta_range));
 C_all = cell(length(alpha_range), length(beta_range));
+A_sliced_all = cell(length(alpha_range), length(beta_range));
+B_sliced_all = cell(length(alpha_range), length(beta_range));
 
 %inten = inten_full;
 %inten_sliced = inten_full_sliced;
@@ -55,29 +57,34 @@ for i = 1:length(alpha_range)
         if (alpha < -beta)
             continue;
         end
-
-        fprintf('Factorizing partitions of I...');
-        [mad_A, outliers_A, mad_B, outliers_B] = ...
-            get_quality_cv_frankenstein(inten_train, inten_train_sliced, inten_full_idx, inten_test_sliced, ...
-            partition_arrays, partition_probes, partition_probes_compl, partition_probes_sliced, partition_probes_sliced_compl, ...
-            alpha, beta, maxIterCnt, eps);
-        fprintf(' Done\n');
         
         [Abest, Bbest, Cbest, Cbestcontrol, Abestsliced, Bbestsliced, reg_best(i, j), test_error(i, j)] = ...
             nonlinear_calibrate_frankenstein_model(inten_train, inten_train_sliced, inten_test_sliced, inten_full_idx, ...
             alpha, beta, maxIterCnt, eps);
         
-        I = inten_train(:, 1:end-2);
-        error = (langmuir_func(Abest, Bbest, Cbest) - I) ./ I .* repmat(C, size(I, 1), 1);
-        bound = quantile(error(:), 0.75);
-        W = ones(size(inten));
-        W = W .* (error <= bound);
-        train_error(i, j) = sum(sum(I .* abs(langmuir_func(Abest, Bbest, Cbest) - I) .* W)) / train_size;
+        for gene_idx = 1:length(inten_train_sliced)
+            I = inten_train_sliced{gene_idx};
+            error = (langmuir_func(Abestsliced{gene_idx}, Bbestsliced{gene_idx}, Cbest(gene_idx, :)) - I) ./ ...
+                I .* repmat(Cbest(gene_idx, :), size(I, 1), 1);
+            bound = quantile(error(:), 0.75);
+            W = ones(size(I));
+            W = W .* (error <= bound);
+            train_error(i, j) = train_error(i, j) + ...
+                sum(sum(I .* abs(langmuir_func(Abestsliced{gene_idx}, Bbestsliced{gene_idx}, Cbest(gene_idx, :)) - I) .* W)) / train_size;
+        end
         
         overfitting(i, j) = test_error(i, j) - train_error(i, j);
         
         C_loo(i, j) = nonlinear_get_LOO_error(inten_train_sliced, Abestsliced, Bbestsliced, Cbest, reg_best(i, j), ...
             alpha, beta, maxIterCnt, eps);
+        
+        
+        fprintf('Factorizing partitions of I...');
+        [mad_A, outliers_A, mad_B, outliers_B] = ...
+            get_quality_cv_frankenstein(inten_train, inten_train_sliced, inten_full_idx, inten_test_sliced, ...
+            partition_arrays, partition_probes, partition_probes_compl, partition_probes_sliced, partition_probes_sliced_compl, ...
+            alpha, beta, maxIterCnt, eps, reg_best(i, j));
+        fprintf(' Done\n');
         
         %fprintf('Factorizing I...');
         %[A, C, Avect, A_sliced, converged_cnt_full(i, j), norm_problems_cnt(i, j)] = calibrate_model_parallel(inten, inten_sliced, ...
@@ -89,7 +96,14 @@ for i = 1:length(alpha_range)
         %    @(I_arg, A_arg) nmf_alpha_beta_fixedA(I_arg, A_arg, alpha, beta, maxIterCnt, 1e-6));
         %fprintf(' Done\n');
         
+        A_all{i, j} = Abest;
+        B_all{i, j} = Bbest;
+        C_all{i, j} = Cbest;
+        
+        A_sliced_all{i, j} = Abestsliced;
+        B_sliced_all{i, j} = Bbestsliced;
+        
         save('cv_frankenstein_res.mat', 'mad_A', 'outliers_A', 'mad_B', 'outliers_B', 'C_loo', ...
-            'train_error', 'test_error', 'overfitting', 'A_all', 'B_all', 'C_all');
+            'train_error', 'test_error', 'overfitting', 'A_all', 'B_all', 'C_all', 'A_sliced_all', 'B_sliced_all');
     end
 end
