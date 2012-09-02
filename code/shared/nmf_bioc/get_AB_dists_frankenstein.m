d29582083bc2714342a0170bc65b839971becdca
riabenko <Lars@larra.(none)>
2012-09-03 03:51:12 +0400

    moving some stuff-2

---
+++ code/shared/nmf_bioc/get_AB_dists_frankenstein.m
@@ -0,0 +1,9 @@
+dist_A = zeros(length(alpha_range), length(beta_range));
+dist_B = zeros(length(alpha_range), length(beta_range));
+
+for i = 1:length(alpha_range)
+    for j = 1:length(beta_range)
+        [dist_A(i,j), dist_B(i,j)] = nonlinear_get_AB_error(inten_train, inten_train_sliced, inten_full_idx, ...
+        partition_arrays, alpha_range(i), beta_range(j), maxIterCnt, eps, reg_best(i, j));
+    end
+end
---
+++ code/shared/nmf_bioc/nonlinear_get_AB_error.m
@@ -0,0 +1,34 @@
+function [dist_A, dist_B] = ...
+        nonlinear_get_AB_error(inten, inten_sliced, inten_genes_idx, ...
+        partition_arrays, ...
+        alpha, beta, maxIterCnt, eps, reg_best)
+    
+    sampleSize = size(inten, 2) - 2;
+    
+    % разбиение по чипам
+    sample1 = inten(:, [partition_arrays (size(inten, 2)-1:size(inten, 2))]);
+    sample2 = inten(:, [setdiff(1:sampleSize, partition_arrays) (size(inten, 2)-1:end)]);
+    sample1_sliced = inten_sliced;
+    sample2_sliced = inten_sliced;
+    for i = 1:length(inten_sliced)
+        sample1_sliced{i} = sample1_sliced{i}(:, partition_arrays);
+        sample2_sliced{i} = sample2_sliced{i}(:, setdiff(1:sampleSize, partition_arrays));
+    end
+
+    fprintf(' 1');
+    [~, ~, ~, Avect1_arrays, Bvect1_arrays] = nonlinear_calibrate_model_short(sample1, sample1_sliced, inten_genes_idx, ...
+        @(I) nonlinear_alpha_beta_LO_reg(I, alpha, beta, maxIterCnt, eps, reg_best, 1));
+    fprintf(' 2');
+    [~, ~, ~, Avect2_arrays, Bvect2_arrays] = nonlinear_calibrate_model_short(sample2, sample2_sliced, inten_genes_idx, ...
+        @(I) nonlinear_alpha_beta_LO_reg(I, alpha, beta, maxIterCnt, eps, reg_best, 1));
+
+    tmp = abs(Avect1_arrays - Avect2_arrays) ./ (Avect1_arrays + Avect2_arrays);
+    tmp = tmp(~isnan(tmp));
+    tmp = tmp(~isinf(tmp));
+    dist_A = mean(tmp);
+    
+    tmp = abs(Bvect1_arrays - Bvect2_arrays) ./ (Bvect1_arrays + Bvect2_arrays);
+    tmp = tmp(~isnan(tmp));
+    tmp = tmp(~isinf(tmp));
+    dist_B = mean(tmp);        
+end
